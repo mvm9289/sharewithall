661caa5d04bf2e4a017d1003fc0e56e55869af60
mvm9289 <mvm9289@fba1f13f-c5d5-eaad-5d2b-cba41ad73aac>
2010-11-06 13:08:13 +0000

    Sockets and reestructuration

--- trunk/scripts/restartSWAServer.sh
+++ trunk/scripts/restartSWAServer.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-kill -9 `jps | grep SWAServerRMI | cut -d ' ' -f 1`
-killall rmiregistry
+kill -9 `jps | grep SWAServer | cut -d ' ' -f 1`
 sleep 1
 cp /home/sharewithall/SWAServer/resources/connection.properties /home/sharewithall/SWAServer/bin/
 cd /home/sharewithall/SWAServer/bin
-rmiregistry 4040 &
-java -Djava.rmi.server.hostname=192.168.1.122 ShareWithAll.Server.RMI.SWAServerRMI &
+java sharewithall.server.SWAServer &
