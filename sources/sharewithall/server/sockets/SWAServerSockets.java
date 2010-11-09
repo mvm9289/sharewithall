d08a93bc58decfeeca30b1e78329d574f0279c8c
ferranrigual <ferranrigual@fba1f13f-c5d5-eaad-5d2b-cba41ad73aac>
2010-11-09 17:57:18 +0000

    Correct IP and Port on function login

--- sources/sharewithall/server/sockets/SWAServerSockets.java
+++ sources/sharewithall/server/sockets/SWAServerSockets.java
@@ -99,7 +99,7 @@ public class SWAServerSockets extends Thread
                             break;
                         case LOGIN:
                             out.writeUTF(String.valueOf(RETURN_VALUE) + ";" +
-                                    String.valueOf(server.login(petition[1], petition[2], petition[3], Boolean.valueOf(petition[4]).booleanValue())));
+                                    String.valueOf(server.login(petition[1], petition[2], petition[3], Boolean.valueOf(petition[4]).booleanValue(), petition[4])));
                             break;
                         case LOGOUT:
                             server.logout(petition[1]);
