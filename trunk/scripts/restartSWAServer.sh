c7f11aabe16192583bda9f673d2734216cb9e25b
mvm9289 <mvm9289@fba1f13f-c5d5-eaad-5d2b-cba41ad73aac>
2010-11-20 20:25:22 +0000

    Server scripts modified and database scripts added

--- trunk/scripts/restartSWAServer.sh
+++ trunk/scripts/restartSWAServer.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-kill -9 `jps | grep SWAServer | cut -d ' ' -f 1`
-sleep 1
-cp /home/sharewithall/SWAServer/resources/connection.properties /home/sharewithall/SWAServer/bin/
-cd /home/sharewithall/SWAServer/bin
-java sharewithall.server.SWAServer &
+stopSWAServer.sh
+sleep 3
+startSWAServer.sh
